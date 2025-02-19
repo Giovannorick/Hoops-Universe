de10276900dc42ac48aac913bd941c4f17e07170
Giovan Norick Yang <giovanoricky@gmail.com>
2025-02-19 19:12:08 +0700

    refactor: remove unnecessary whitespace in JavaScript files

--- script/purchase.js
+++ script/purchase.js
@@ -1,7 +1,6 @@
 $(document).ready(function () {
   const productName = localStorage.getItem("productName");
   const productPrice = localStorage.getItem("productPrice");
-
   if (productName && productPrice) {
     $("#product-name span").text(productName);
     $("#product-price span").text(productPrice);
@@ -9,15 +8,11 @@ $(document).ready(function () {
     $("#product-name span").text("No product selected");
     $("#product-price span").text("-");
   }
-
   $("#purchase-form").on("submit", function (e) {
     e.preventDefault();
-
     alert("Thank you for your transaction!");
-
     localStorage.removeItem("productName");
     localStorage.removeItem("productPrice");
-
     window.location.href = "index.html";
   });
 });
--- script/toggleMenu.js
+++ script/toggleMenu.js
@@ -1,7 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
   const menuToggle = document.querySelector(".menu-toggle");
   const navContainer = document.querySelector(".nav-container");
-
   menuToggle.addEventListener("click", function () {
     navContainer.classList.toggle("show");
   });
